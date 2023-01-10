a31c3152619256597f49317c61623aa9acf26475
Anton Iankin <kerjen01@gmail.com>
2023-01-10 21:45:01 +0300

    [FIX] Added exports to app_kit.dart

--- lib/app_kit.dart
+++ lib/app_kit.dart
@@ -1,3 +1,22 @@
 library app_kit;
 
 
+// General
+export 'row.dart';
+export 'gesture_detector.dart';
+export 'dashed_line.dart';
+export 'empty_widget.dart';
+export 'column.dart';
+export 'bouncing_gesture_detector.dart';
+export 'app_bar.dart';
+
+// Animations
+export 'animations/no_scaling_animator.dart';
+
+// Behaviors
+export 'behaviors/bouncing_scroll_behavior.dart';
+
+// Helpers
+export 'helpers/popup_helper.dart';
+export 'helpers/sheet_helper.dart';
+
