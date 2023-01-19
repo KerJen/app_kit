4ca22eff143a42062233e8b9333ad8b26443d00c
Anton Iankin <kerjen01@gmail.com>
2023-01-19 15:36:28 +0300

    [FIX] Added return type for the empty widget

--- lib/empty_widget.dart
+++ lib/empty_widget.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/material.dart';
 
-get emptyWidget => SizedBox(key: UniqueKey(), height: 0, width: 0);
+SizedBox get emptyWidget => SizedBox(key: UniqueKey(), height: 0, width: 0);
